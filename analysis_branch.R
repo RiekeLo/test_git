f6f0a228904e1484dbcbb5b402c78c9854a5223f
RiekeLo <rieke.lo.madsen@nmbu.no>
2022-11-17 10:41:40 +0100

    I added some valuable additions

--- analysis_branch.R
+++ analysis_branch.R
@@ -1,1 +1,6 @@
 # analysis in branch
+#this is a comment that I added beacuse I thought that it was nice
+#I hope that you will accept it
+#Thank you :)))
+#And some rainbow parantheses:
+(((((())))))
