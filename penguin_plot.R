3a64f99ff0be776aa81d0a65e968f9b526d8002c
driadash <darshyr@gmail.com>
2022-11-17 09:03:01 +0100

    test

---
+++ penguin_plot.R
@@ -0,0 +1,9 @@
+#' Size measurements for adult foraging penguins near Palmer Station, Antarctica
+#'
+#' Includes measurements for penguin species, island in Palmer Archipelago,
+#' size (flipper length, body mass, bill dimensions), and sex.
+#' This is a subset of \code{\link{penguins_raw}}.
+
+#make plot
+
+
